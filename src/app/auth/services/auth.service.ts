759dec9a8fa8a7a3fddac4e3a53c6169f37698dd
UrielPM <upm1808@gmail.com>
2021-12-08 16:32:05 -0600

    primer  commit

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { AuthResponse } from '../pages/interfaces/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+  private baseUrl: string = environment.baseUrl;
+
+  constructor( private http: HttpClient) { }
+
+  login( email: string, password: string ){
+    
+    const url = `${ this.baseUrl}/auth`;
+    const body = { email, password};
+    
+    return this.http.post<AuthResponse>(url, body);
+    
+  }
+
+}
